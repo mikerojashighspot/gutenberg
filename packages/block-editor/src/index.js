4df9565fdd7a0c5bedbb5b76af4b2f02b4d7c88c
Adam Zieliński <adam@adamziel.com>
2020-06-24 15:44:08 +0200

    Extract useDisplayUrl into a separate module

--- packages/block-editor/src/index.js
+++ packages/block-editor/src/index.js
@@ -9,8 +9,11 @@ import '@wordpress/keyboard-shortcuts';
 /**
  * Internal dependencies
  */
-import { AlignmentHookSettingsProvider as __experimentalAlignmentHookSettingsProvider } from './hooks';
-export { __experimentalAlignmentHookSettingsProvider };
+import {
+	AlignmentHookSettingsProvider as __experimentalAlignmentHookSettingsProvider,
+	useDisplayUrl,
+} from './hooks';
+export { useDisplayUrl, __experimentalAlignmentHookSettingsProvider };
 export * from './components';
 export * from './utils';
 export { storeConfig } from './store';
